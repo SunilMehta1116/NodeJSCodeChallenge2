232ebdca823304ffb56ebd5c52eacb26a4c514b4
unknown <sunilm@PC1565.itp.objectfrontier.com>
2014-12-22 18:42:10 +0530

    Added new html file for private chat and improvised group chat flow

--- chat.js
+++ chat.js
@@ -5,21 +5,60 @@ var express = require('express'),
 	//,filePath = require('./Chatroom.html');
 	
 	
-	
+var userList = [];
 app.get('/',function(req,res){
 	//res.sendFile(filePath);
 	res.sendfile('Chatroom.html');
 });
 
+app.get('/personalChat',function(req,res){
+	//res.sendFile(filePath);
+	res.sendfile('personalChat.html');
+});
+
+
 io.on('connection',function(socket){
-	console.log('a user has connected');
-	socket.on('cm',function(msg){
+	
+
+	console.log('a user has connected');	
+	
+		socket.on('cm',function(msg){
+		
+		
+		//message emitting
 		io.emit('cm',msg);
-		console.log(msg);
+		//console.log(msg);
 	});
-	socket.on('disconnect',function(msg){
-		console.log('a user has disconnected');
+	
+	socket.on('ul',function(user){
+		console.log(userList);
+		//if(userList.indexOf(user) == -1){
+			userList.push(user);
+		//}
+		console.log(userList);
+		io.emit('ul',userList);
 	});
+
+	socket.on('depart',function(user){
+	//var user = msg.split(';')[0];
+	
+		if(userlist.indexof(user) != -1){
+			userlist.splice(userlist.indexof(user,1));
+		}
+		console.log(user + ':  user has disconnected');
+	});
+	
+	socket.on('disconnect',function(user){
+	//var user = msg.split(';')[0];
+	
+	//	if(userList.indexOf(user) != -1){
+	//		userList.splice(userList.indexOf(user,1));
+	//	}
+		console.log(user + ':  user has disconnected');
+	});
+	
+	
+	
 	
 });
 
