0385e94a9a6e7f408781e8295917e792234e1480
unknown <sunilm@PC1565.itp.objectfrontier.com>
2014-12-24 17:16:50 +0530

    more add on to the functionlaity of private chat

--- chat.js
+++ chat.js
@@ -13,19 +13,84 @@ app.get('/',function(req,res){
 });
 
 app.get('/personalChat',function(req,res){
+	
 	//res.sendFile(filePath);
+	var pvtChatUser = req.query.chatMan;
 	res.sendfile('personalChat.html');
 });
 
 
-io.on('connection',function(socket){
-	
-	socketList.push(socket);
+var previousChat = [];
 
-		socket.on('cm',function(msg){
+var findSocketIndexByUser = function(user,socketsList){
+	console.log('inside new function');
+	for(var i=0,j=socketList.length;i<j;i++){
+			if(typeof socketList[i] == 'undefined'){
+				console.log('logged undefined ' + socketList[i]);
+			}else{
+				 if(socketList[i].user === user){
+					return 	socketList[i];
+					}
+				}
+			}
+	}
+
+io.on('connection',function(socket){
+	socket.user = 'noUser'
+	socketList.push(socket);
+	//console.log(socket.user);
+	//console.log(previousChat.length);
+	
+	//previous 10 chat lines will show to new chat user.
+	for(var i=0,j=previousChat.length;i<j;i++){
+			socket.emit('cm',previousChat[i]);
+	}
+		
+	////chat request emitted from a user will emit request to requested user here
+	socket.on('pmReq',function(msg){
+	console.log('PM Req, msg: ' + msg);
+	if( msg.indexOf('accepted') == -1 && msg.indexOf('denied') == -1 ){
+		console.log('PM Req - new req');
+			var indexReqSocket = socketList.indexOf(socket);
+			var reqUser = socketList[indexReqSocket].user
+			console.log(reqUser + ": requested to chat in person to :" );
+			console.log(msg);
+			
+			//this loop to find the user to whom PM request has to be made
+			console.log("number of sockets active:" +socketList.length);
+			for(var i=0,j=socketList.length;i<j;i++){
+			if(typeof socketList[i] == 'undefined'){
+				console.log('logged undefined ' + socketList[i]);
+			}else{
+				 if(socketList[i].user === msg){
+						socketList[i].emit('pmReq', reqUser);
+					}
+				}
+			}
+			
+			
+			console.log(socketList.length);
+		} else if( msg.indexOf('accepted') != -1){ 
+			var reqUser = msg.split('-')[1];
+			var isocket = findSocketIndexByUser(reqUser);
+			isocket.emit('pmReq','accepted');
+		} else if(msg.indexOf('denied') != -1) {
+			var isocket = findSocketIndexByUser(reqUser);
+			isocket.emit('pmReq','accepted');
+		}
+	});
+	
+	
+	socket.on('cm',function(msg){
 		//var user = msg.split(':')[0];	
 		//socket.user = user;
 		//message emitting
+		previousChat.push(msg);
+		if (previousChat.length > 10)
+			previousChat.shift();
+			
+			
+		
 		io.emit('cm',msg);
 		//console.log(msg);
 	});
@@ -42,23 +107,12 @@ io.on('connection',function(socket){
 		io.emit('ul',userList);
 	});
 
-	socket.on('depart',function(user){
-	//var user = msg.split(';')[0];
-	
-		if(userlist.indexof(user) != -1){
-			userlist.splice(userlist.indexof(user,1));
-		}
-		console.log(user + ':  user has disconnected');
-	});
 	
 	socket.on('disconnect',function(user){
-	//var user = msg.split(';')[0];
-	
-	//	if(userList.indexOf(user) != -1){
-	//		userList.splice(userList.indexOf(user,1));
-	//	}
+
+		console.log(userList.indexOf(socket.user));
 		if(userList.indexOf(socket.user) != -1){
-		userList.splice(userList.indexOf(socket.user,1));
+			userList.splice(userList.indexOf(socket.user),1);
 		}
 		var i = socketList.indexOf(socket);
 		delete socketList[i];
